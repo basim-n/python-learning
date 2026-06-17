b5b76e6340f2a920a03cd28230d53938f47b6c9f
Basim <muhammedbasim616@gmail.com>
2026-06-17 13:04:42 +0530

    DSA-BINARY SEARCH

---
+++ dsa9.py
@@ -0,0 +1,15 @@
+def binary_search(numbers, target):
+    left=0
+    right=len(numbers)-1
+    while left<=right:
+        mid=(left+right)//2
+        if numbers[mid]==target:
+            return mid
+        elif numbers[mid]>target:
+            right=mid-1
+        else:
+            left=mid+1
+    return -1
+numbers = [1, 3, 5,7,  9, 11, 13]
+target = 7
+print(binary_search(numbers,target))
