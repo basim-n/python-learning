bf180789e6979029d4cd316e0ace9a65aac1188d
Basim <muhammedbasim616@gmail.com>
2026-07-10 17:40:10 +0530

    Weather app using OpenWeatherMap API

--- weather.py
+++ weather.py
@@ -1,5 +1,5 @@
 import requests
-API_KEY="ea8069c10c90968095c45897a8de2516"
+API_KEY=""
 BASE_URL= "http://api.openweathermap.org/data/2.5/weather"
 
 
@@ -11,8 +11,22 @@ def weather(city):
     }
     response=requests.get(BASE_URL,params=params)
     return response.json()
-city=input("Enter your city:")
-data=weather(city)
-print(data)
-
+def display_weather(data):
+        if int(data['cod']) !=200:
+              print(f"ERROR:{data['message']}")
+        else:
+              print("-"*12)
+              print(f"CITY:{data['name']},{data['sys']['country']}")
+              print(f"TEMPERATURE:{data['main']['temp']} oC")
+              print(f"Feels Like:{data['main']['feels_like']}")
+              print(f"HUMIDITY: {data['main']['humidity']}")
+              print(f"CONDITION:{data['weather'][0]['description']}")
+              print("-"*12)
+while True:
+      city=input("Enter city or 'quit' to exit:")
+      if city!='quit':
+           data=weather(city)
+           display_weather(data)
+      else:
+            break
 
