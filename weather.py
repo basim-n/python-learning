f355077e07f3f02dcf0a98de81da1d06bffa5caf
Basim <muhammedbasim616@gmail.com>
2026-07-12 17:25:12 +0530

    Weather app - current weather and 5-day forecast

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import requests
-API_KEY=""
+API_KEY="bd9fe17c7cfdcd1fa85dd5d32a4ab112"
 BASE_URL= "http://api.openweathermap.org/data/2.5/weather"
+FORECAST_URL="http://api.openweathermap.org/data/2.5/forecast"
 
 
 def weather(city):
@@ -22,11 +23,41 @@ def display_weather(data):
               print(f"HUMIDITY: {data['main']['humidity']}")
               print(f"CONDITION:{data['weather'][0]['description']}")
               print("-"*12)
+def get_forecast(city):
+      params={
+        "q":city,
+        "appid":API_KEY,
+        "units":"metric"
+    }
+      response=requests.get(FORECAST_URL,params=params)
+      return response.json()
+def display_forecast(data):
+      if str(data['cod'])!='200':
+            print(f"ERROR:{data['message']}")
+            return
+      
+      print(f"\n5-Day Forecast for {data['city']['name']}, {data['city']['country']}")
+      print("-" * 40)
+      for entry in data['list']:
+            if '12:00:00' in entry['dt_txt']:
+                        print(f"{entry['dt_txt']} | {entry['main']['temp']}oC | {entry['weather'][0]['description']}")
+
 while True:
-      city=input("Enter city or 'quit' to exit:")
-      if city!='quit':
-           data=weather(city)
-           display_weather(data)
-      else:
+      print(f"=== WEATHER APP===")
+      print("1.Current Weather")
+      print("2.5 day forecast")
+      print("3.Quit")
+      ch=input("Enter a choice:")
+      if ch=='1':
+             city=input("Enter city or 'quit' to exit:")
+             data=weather(city)
+             display_weather(data)
+      elif ch=='2':
+            city=input("Enter city name:")
+
+            data=get_forecast(city)
+            display_forecast(data)
+      elif ch =='3':
+            print("GOODBYE!")
             break
 
