87b7113fbe7137796f0f9658c3a1f05142dc936f
Basim <muhammedbasim616@gmail.com>
2026-07-31 16:47:50 +0530

    LEETCODE-LONGEST CONSECUTIVE SEQUENCE USING HASHING

---
+++ longestconsecutiveseq.py
@@ -0,0 +1,19 @@
+nums=[100,4,200,1,3,2]
+st=set(nums)
+
+
+longest=0
+for i in st:
+    if i-1 not in st:
+        current=i
+        length=1
+        while current+1 in st:
+            current+=1#2
+            length+=1#2
+        longest=max(length,longest)
+print(longest)
+
+
+        
+
+    
