c962a4b6a99f5e95f69f880c1b17b9e560d179c1
Basim <muhammedbasim616@gmail.com>
2026-08-03 22:59:54 +0530

    LEETCODE-SEARCH ON ROTATED SORTED ARRAY

---
+++ sortedrotatedarray.py
@@ -0,0 +1,34 @@
+def sortedarray(nums,target):
+    
+    
+    left = 0
+    right = len(nums)-1
+    
+
+    
+    while left <= right:
+
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        
+        if nums[left] <= nums[mid]:
+
+            
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        else:
+
+            
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return -1
+result=sortedarray(nums = [7,8,1,2,3,4,5,6],target = 2)
+print(result)
